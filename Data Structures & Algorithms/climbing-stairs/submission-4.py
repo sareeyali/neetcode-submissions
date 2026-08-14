0cfd447165f4c9d6b95c4756f2c454033f6ec2e0
Sareeya Ali <sayli@ucdavis.edu>
2026-08-14 16:03:43 -0700

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        one = 1
+        two = 2
+        for i in range(n-2):
+            nxt = one + two
+            one = two
+            two = nxt
+        return two
