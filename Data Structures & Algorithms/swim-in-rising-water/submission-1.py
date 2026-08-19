504584c9620bc104e08a6d89013728f23059daeb
Sareeya Ali <sayli@ucdavis.edu>
2026-08-19 12:06:24 -0700

    Add: swim-in-rising-water - submission-1

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        res = 0
+        heap = [[grid[0][0], 0, 0]]
+        dirs = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+        m = len(grid)
+        n = len(grid[0])
+        seen = set((0, 0))
+
+        while heap:
+            h, r, c = heapq.heappop(heap)
+            if (r == m - 1 and c == n - 1):
+                return h
+            seen.add((r, c))
+            for dr, dc in dirs:
+                nr = dr + r
+                nc = dc + c
+                if (0 <= nr < m and 0 <= nc < n
+                and (nr, nc) not in seen):
+                    curH = max(h, grid[nr][nc])
+                    heapq.heappush(heap, [curH, nr, nc])
+        return -1
